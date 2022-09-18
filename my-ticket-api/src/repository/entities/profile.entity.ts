c2094e8f7d690226262b0c91ae1296df20f7c7ba
RafaDraw <www.rafadraw@gmail.com>
2022-09-18 16:36:57 -0300

    crate entities

---
+++ my-ticket-api/src/repository/entities/profile.entity.ts
@@ -0,0 +1,45 @@
+import { IProfile } from '../types';
+
+export class Profile {
+	private _id: number;
+	private _name: string;
+	private _description: string;
+	private _active: boolean;
+
+	constructor(data: IProfile, id?: number) {
+		Object.assign(this, data);
+
+		if (id) this.id = id;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	set id(value: number) {
+		this._id = Number(value);
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(value: string) {
+		this._name = value;
+	}
+
+	get description() {
+		return this._description;
+	}
+
+	set description(value: string) {
+		this._description = value;
+	}
+	get active() {
+		return this._active;
+	}
+
+	set active(value: boolean) {
+		this._active = value;
+	}
+}
--- my-ticket-api/src/repository/entities/user.entity.ts
+++ my-ticket-api/src/repository/entities/user.entity.ts
@@ -1,3 +1,73 @@
-export class User {
+import { IUser } from 'src/repository/types';
 
+export class User {
+	private _id: number;
+	private _firstName: string;
+	private _lastName: string;
+	private _cpf: string;
+	private _email: string;
+	private _profileId: number;
+	private _deletedAt?: Date;
+
+	constructor(data: IUser, id?: number) {
+		Object.assign(this, data);
+
+		if (id) this.id = id;
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	set id(value: number) {
+		this._id = Number(value);
+	}
+
+	get firstName(): string {
+		return this._firstName;
+	}
+
+	set firstName(value: string) {
+		this._firstName = value;
+	}
+
+	get lastName(): string {
+		return this._lastName;
+	}
+
+	set lastName(value: string) {
+		this._lastName = value;
+	}
+
+	get cpf(): string {
+		return this._cpf;
+	}
+
+	set cpf(value: string) {
+		this._cpf = value;
+	}
+
+	get email(): string {
+		return this._email;
+	}
+
+	set email(value: string) {
+		this._email = value;
+	}
+
+	get profileId() {
+		return this._profileId;
+	}
+
+	set profileId(value: number) {
+		this._profileId = Number(value);
+	}
+
+	get deletedAt() {
+		return this._deletedAt;
+	}
+
+	set deletedAt(value: Date | undefined) {
+		this._deletedAt = value ? new Date(value) : value;
+	}
 }
