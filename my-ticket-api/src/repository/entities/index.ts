0a540b31495f1888ee341db30910c33e3509e58f
RafaDraw <www.rafadraw@gmail.com>
2022-09-19 12:29:41 -0300

    Crate file interfece

--- my-ticket-api/src/repository/entities/index.ts
+++ my-ticket-api/src/repository/entities/index.ts
@@ -2,3 +2,4 @@ export * from './user.entity';
 export * from './profile.entity';
 export * from './category.entity';
 export * from './event.entity';
+export * from './file.entity';
--- my-ticket-api/src/repository/types/index.ts
+++ my-ticket-api/src/repository/types/index.ts
@@ -2,3 +2,4 @@ export * from './user';
 export * from './profile';
 export * from './category';
 export * from './event';
+export * from './file';
