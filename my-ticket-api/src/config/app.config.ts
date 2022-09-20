1d7eb9a83d9fbf0ccb34d50d6c6f19f37eeaf5be
RafaDraw <www.rafadraw@gmail.com>
2022-09-20 17:55:32 -0300

    Create Environment congig

--- my-ticket-api/src/config/app.config.ts
+++ my-ticket-api/src/config/app.config.ts
@@ -1,1 +1,15 @@
-export class AppConfig {}
+import express, { Application } from 'express';
+
+export class AppConfig {
+  private _express: Application;
+
+  constructor() {
+    this._express = express();
+  }
+
+  get express() {
+    return this._express;
+  }
+
+  configExpress() {}
+}
