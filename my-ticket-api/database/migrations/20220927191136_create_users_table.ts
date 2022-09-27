7fb2f3c284c39c7e3beef482060583f3621c1626
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 16:55:02 -0300

    Add param on Users migration file

--- my-ticket-api/database/migrations/20220927191136_create_users_table.ts
+++ my-ticket-api/database/migrations/20220927191136_create_users_table.ts
@@ -4,10 +4,10 @@ import TableBuilder = Knex.TableBuilder;
 export async function up(knex: Knex): Promise<void> {
 	return knex.schema.createTable('users', (table: TableBuilder) => {
 		table.increments('id').primary();
-		table.string('first_name').nullable();
-		table.string('last_name').nullable();
+		table.string('first_name', 100).nullable();
+		table.string('last_name', 50).nullable();
 		table.string('cpf').notNullable().unique();
-		table.string('email').notNullable().unique();
+		table.string('email', 1000).notNullable().unique();
 		table.integer('profile_id').unsigned().references('id').inTable('profiles').notNullable();
 		table.timestamp('deleted_at').nullable();
 	});
