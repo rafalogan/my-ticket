2091a0b586e59ee41a6d8654fbe66619318117ce
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 17:05:14 -0300

    Set Categories migration file

--- my-ticket-api/database/migrations/20220927195628_create_users_categories_table.ts
+++ my-ticket-api/database/migrations/20220927195628_create_users_categories_table.ts
@@ -1,10 +1,18 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('categories', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('name', 50).notNullable();
+		table.string('description', 1000).nullable();
+		table.string('url').nullable();
+		table.boolean('status').notNullable().defaultTo(true);
+		table.integer('parent_id').unsigned().references('id').inTable('categories').nullable();
+		table.integer('user_id').unsigned().references('id').inTable('users').nullable();
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('categories');
 }
-
