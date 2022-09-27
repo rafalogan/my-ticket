d775ada67d40f054cee615b68887ce5b6f9943fa
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 19:09:20 -0300

    Set phones migration file

--- my-ticket-api/database/migrations/20220927212402_create_phones_table.ts
+++ my-ticket-api/database/migrations/20220927212402_create_phones_table.ts
@@ -1,5 +1,16 @@
 import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('phones', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('type', 50).notNullable();
+		table.string('number').notNullable();
+		table.integer('user_id').unsigned().references('id').inTable('users').nullable();
+		table.integer('place_id').unsigned().references('id').inTable('places').nullable();
+	});
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('phones');
+}
