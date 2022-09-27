ecc5d888d8660f0dc0c98ed494ea1544497de909
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 18:01:40 -0300

    Set Adress migration file

--- my-ticket-api/database/migrations/20220927204655_create_adress_table.ts
+++ my-ticket-api/database/migrations/20220927204655_create_adress_table.ts
@@ -1,10 +1,15 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('adress', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('zip_code', 45).notNullable();
+		table.string('street', 100).notNullable();
+		table.string('number', 50).nullable();
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('adress');
 }
-
