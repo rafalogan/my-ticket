14317b0b6417456f69cf9e625c30f7d2ac8717ff
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 16:11:01 -0300

    Set create profile table

--- my-ticket-api/database/migrations/20220927185244_create_profile_table.ts
+++ my-ticket-api/database/migrations/20220927185244_create_profile_table.ts
@@ -1,10 +1,15 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('profiles', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('name').notNullable().unique();
+		table.string('description').nullable();
+		table.boolean('active').notNullable().defaultTo(true);
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('profiles');
 }
-
