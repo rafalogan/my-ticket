bd0abc97af64e83ad8ae90363ceae42f424590e8
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 16:28:16 -0300

    Set migration create users table

--- my-ticket-api/database/migrations/20220927191136_create_users_table.ts
+++ my-ticket-api/database/migrations/20220927191136_create_users_table.ts
@@ -1,10 +1,18 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('users', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('first_name').nullable();
+		table.string('last_name').nullable();
+		table.string('cpf').notNullable().unique();
+		table.string('email').notNullable().unique();
+		table.integer('profile_id').unsigned().references('id').inTable('profiles').notNullable();
+		table.timestamp('deleted_at').nullable();
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('users');
 }
-
