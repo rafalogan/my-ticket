c0358dd737d2dbfe80fb10b758a45da524b08abf
RafaDraw <www.rafadraw@gmail.com>
2022-09-27 17:36:11 -0300

    Set Events migration file

--- my-ticket-api/database/migrations/20220927201139_create_events_table.ts
+++ my-ticket-api/database/migrations/20220927201139_create_events_table.ts
@@ -1,10 +1,19 @@
-import { Knex } from "knex";
+import { Knex } from 'knex';
 
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('events', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('title', 100).notNullable();
+		table.string('sub_title', 150).nullable();
+		table.binary('content').notNullable();
+		table.string('type', 50).notNullable();
+		table.integer('category_id').unsigned().references('id').inTable('categories').notNullable();
+		table.integer('user_id').unsigned().references('id').inTable('users').nullable();
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('events');
 }
-
