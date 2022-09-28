74f4006636e48cdc3fd8a772bb93a71a5a21d109
RafaDraw <www.rafadraw@gmail.com>
2022-09-28 18:50:53 -0300

    Set TickestPerSales migration file

--- my-ticket-api/database/migrations/20220928214604_create_tickets_per_sales_table.ts
+++ my-ticket-api/database/migrations/20220928214604_create_tickets_per_sales_table.ts
@@ -1,10 +1,14 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('tickets_per_sales', (table: TableBuilder) => {
+		table.integer('ticket_id').unsigned().references('id').inTable('tickets').notNullable();
+		table.integer('sale_id').unsigned().references('id').inTable('sales').notNullable();
+		table.string('armchair', 100).notNullable();
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('tickets_per_sales');
 }
-
