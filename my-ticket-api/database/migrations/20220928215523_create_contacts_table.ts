735bdc1bce1fbf8da6650676ee55cf3d9e867526
RafaDraw <www.rafadraw@gmail.com>
2022-09-28 19:09:21 -0300

    Set Contacts migration file

--- my-ticket-api/database/migrations/20220928215523_create_contacts_table.ts
+++ my-ticket-api/database/migrations/20220928215523_create_contacts_table.ts
@@ -1,10 +1,19 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
 export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('contacts', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('name', 100).notNullable();
+		table.string('email', 100).notNullable();
+		table.string('phone', 50).nullable();
+		table.string('subject', 1000).notNullable();
+		table.binary('message').notNullable();
+		table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+		table.integer('sale_id').unsigned().references('id').inTable('sales').nullable();
+	});
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('contacs');
 }
-
