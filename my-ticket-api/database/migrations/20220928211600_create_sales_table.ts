517ecfebba0d5f34f49cb4f1e903dcb750f61ea9
RafaDraw <www.rafadraw@gmail.com>
2022-09-28 18:37:15 -0300

    Set Sales migration file

--- my-ticket-api/database/migrations/20220928211600_create_sales_table.ts
+++ my-ticket-api/database/migrations/20220928211600_create_sales_table.ts
@@ -1,5 +1,18 @@
 import { Knex } from 'knex';
+import TableBuilder = Knex.TableBuilder;
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+	return knex.schema.createTable('sales', (table: TableBuilder) => {
+		table.increments('id').primary();
+		table.string('code', 1000).notNullable();
+		table.integer('discount').nullable();
+		table.integer('amount').notNullable();
+		table.integer('unitary_value').notNullable();
+		table.integer('total').notNullable();
+		table.integer('user_id').unsigned().references('id').references('users').notNullable();
+	});
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+	return knex.schema.dropTable('sales');
+}
