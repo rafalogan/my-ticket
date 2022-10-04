5d1d811765702465472143d9dfb1dc6f2a348034
RafaDraw <www.rafadraw@gmail.com>
2022-10-04 12:27:15 -0300

    exec Auth Module

--- my-ticket-api/src/core/factories/modules.factory.ts
+++ my-ticket-api/src/core/factories/modules.factory.ts
@@ -1,11 +1,17 @@
 import { Application } from 'express';
 import { AuthConfig } from 'src/config';
 import { notfoundRoute } from 'src/core/routes/notfound.route';
+import { ServicesFactory } from 'src/core/factories/services.factory';
+import { AuthModule } from 'src/modules/auth';
 
 export class ModulesFactory {
-	constructor(private app: Application, private auth: AuthConfig) {}
+	private authModule: AuthModule;
+	constructor(private app: Application, private auth: AuthConfig, services: ServicesFactory) {
+		this.authModule = new AuthModule({ service: services.authService, app, auth });
+	}
 
 	exec() {
+		this.authModule.exec();
 		this.app.use(notfoundRoute);
 	}
 }
