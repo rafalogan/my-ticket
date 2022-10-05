cbd374a62e84db6ad4bb69949c6d5a0881572e1d
RafaDraw <www.rafadraw@gmail.com>
2022-10-05 16:49:21 -0300

    No console Autheticate

--- my-ticket-api/src/config/auth.config.ts
+++ my-ticket-api/src/config/auth.config.ts
@@ -23,10 +23,7 @@ export class AuthConfig {
 
 		passport.use(strategy);
 		return {
-			authenticate: () => {
-				onLog('authenticate');
-				return passport.authenticate('jwt', { session });
-			},
+			authenticate: () => passport.authenticate('jwt', { session }),
 		};
 	}
 
