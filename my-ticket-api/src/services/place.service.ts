a9e257c2625ade784b731b7358e5147f44c66996
RafaDraw <www.rafadraw@gmail.com>
2022-10-08 22:52:50 -0300

    Create Place Service

---
+++ my-ticket-api/src/services/place.service.ts
@@ -0,0 +1,8 @@
+import { BaseService } from 'src/core/abstracts';
+import { BaseServiceOptions } from 'src/repositories/types';
+
+export class PlaceService extends BaseService {
+	constructor(options: BaseServiceOptions) {
+		super(options);
+	}
+}
