d2b7a6f9d3eef48b675d2d3c6079febefc94f6f1
RafaDraw <www.rafadraw@gmail.com>
2022-10-12 22:50:35 -0300

    Create Sale Service

--- my-ticket-api/src/services/sale.service.ts
+++ my-ticket-api/src/services/sale.service.ts
@@ -1,6 +1,7 @@
 import { BaseService } from 'src/core/abstracts';
-import { BaseServiceOptions } from 'src/repositories/types';
+import { BaseServiceOptions, ISale, List, ReadOptions } from 'src/repositories/types';
 import { Sale } from 'src/repositories/entities';
+import { DatabaseException, responseDataBaseCreate, responseDataBaseUpdate } from 'src/utils';
 
 export class SaleService extends BaseService {
 	constructor(options: BaseServiceOptions) {
@@ -8,6 +9,35 @@ export class SaleService extends BaseService {
 	}
 
 	create(item: Sale) {
-		return super.create(item);
+		return super
+			.create(item)
+			.then(res => responseDataBaseCreate(res, item))
+			.catch(err => err);
+	}
+
+	update(id: number, values: Sale) {
+		return super
+			.update(id, values)
+			.then(res => responseDataBaseUpdate(res, values))
+			.catch(err => err);
+	}
+
+	findAll(options?: ReadOptions) {
+		return super
+			.findAll(options)
+			.then(res => (!res || res instanceof DatabaseException ? res : this.setSales(res)))
+			.catch(err => err);
+	}
+
+	findOneById(id: number, options?: ReadOptions) {
+		return super
+			.findOneById(id, options)
+			.then(res => (!res || res instanceof DatabaseException ? res : new Sale(res)))
+			.catch(err => err);
+	}
+
+	private setSales(value: List<ISale>) {
+		const data = value.data.map(s => new Sale(s));
+		return { ...value, data };
 	}
 }
