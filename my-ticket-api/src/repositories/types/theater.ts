3f25369845cf9b68488172046254e7e017634ad8
RafaDraw <www.rafadraw@gmail.com>
2022-10-13 19:36:31 -0300

    Refactor Theater Interface

--- my-ticket-api/src/repositories/types/theater.ts
+++ my-ticket-api/src/repositories/types/theater.ts
@@ -3,5 +3,11 @@ import { IID } from './shared';
 export interface ITheater extends IID {
 	name: string;
 	description?: string;
+	sectionsNumber?: number;
+	sectionsType?: string;
+	rawsPerSection?: number;
+	rowsType?: string;
+	capacity: number;
+	addressedSeats: boolean;
 	placeId: number;
 }
