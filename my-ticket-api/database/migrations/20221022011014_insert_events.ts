0e31f6982cdd21791a24ee1570d8dea5c304682f
RafaDraw <www.rafadraw@gmail.com>
2022-10-22 19:51:39 -0300

    fix Dev database

--- my-ticket-api/database/migrations/20221022011014_insert_events.ts
+++ my-ticket-api/database/migrations/20221022011014_insert_events.ts
@@ -1,9 +1,11 @@
 import { Knex } from 'knex';
-import EventsDefault from 'database/assets/default-events.json';
+import EventsDefault from '../assets/default-events.json';
 
 export async function up(knex: Knex): Promise<void> {
 	const parser = EventsDefault.map((e: any) => {
 		e.release_date = new Date(e.release_date);
+		e.popularity = e.popularity * 1000;
+		e.vote_average = e.vote_average * 10;
 		return e;
 	});
 
--- my-ticket-api/database/migrations/20221022204030_insert_files.ts
+++ my-ticket-api/database/migrations/20221022204030_insert_files.ts
@@ -4,11 +4,11 @@ import DefaultFiles from '../assets/default-files.json';
 export async function up(knex: Knex): Promise<void> {
 	const prefix = process.env.ENABLE_HTTPS === 'true' ? 'https://' : 'http://';
 	const prepare = DefaultFiles.map(file => {
-		file.url = `${prefix}${process.env.HOST}:${Number(process.env.PORT)}${file.url}`;
+		file.url = file.type.includes('youtube') ? file.url : `${prefix}${process.env.HOST}:${Number(process.env.PORT)}/media${file.url}`;
 		return file;
 	});
 
-	return knex.batchInsert('theaters', prepare as any);
+	return knex.batchInsert('files', prepare as any);
 }
 
 export async function down(knex: Knex): Promise<void> {
